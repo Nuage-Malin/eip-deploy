d4b2f51b3db14efea915f6aee4867f5755cc7fbe
Vincent Andrieu <vincent.andrieu@epitech.eu>
2022-04-23 01:28:23 +0200

    Add front builder

---
+++ jenkins/builders/front.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+check_exit_failure()
+{
+    if [ $? -ne 0 ]
+    then
+        echo "$1" 1>&2
+        exit 1
+    fi
+}
+
+# Build docker images
+docker build -t localhost:5000/eip-frontend:latest .
+check_exit_failure "Fail to build"
+
+# Push docker images in the docker registry
+docker push localhost:5000/eip-frontend:latest .
+check_exit_failure "Fail to push"
+
+# Pull docker images in the cluster
+kind load docker-image --name eip-frontend "localhost:5000/eip-frontend:latest"
+check_exit_failure "Fail to pull docker image in the cluster"
+
+# Deploy kubernetes
+## Apply client
+# sed -ie "s/THIS_STRING_IS_REPLACED_DURING_BUILD/$(date)/g" production/client/kubernetes/client.deployment.yml
+kubectl apply -f kubernetes/
+check_exit_failure "Fail to apply"
