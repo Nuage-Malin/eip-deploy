af5cce264972d07713f64873a82a149fa26b0263
Vincent Andrieu <vincent.andrieu@epitech.eu>
2022-07-08 00:50:26 +0200

    Add a node as Jenkins agent to run jobs

--- jenkins/agent/builders/back.sh
+++ jenkins/agent/builders/back.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 check_exit_failure()
 {
@@ -10,18 +10,12 @@ check_exit_failure()
 }
 
 # Build docker images
-docker build -t localhost:5000/eip-frontend:latest .
+docker build -t localhost:5000/eip-backend:latest .
 check_exit_failure "Fail to build"
 
 # Push docker images in the docker registry
-docker push localhost:5000/eip-frontend:latest
+docker push localhost:5000/eip-backend:latest
 check_exit_failure "Fail to push latest tag"
-
-docker image rm localhost:5000/eip-frontend:latest
-if [ $? -ne 0 ]
-then
-    echo "Fail to delete docker images" 1>&2
-fi
 
 # Deploy kubernetes
 sed -ie "s/THIS_STRING_IS_REPLACED_DURING_BUILD/$(date)/g" kubernetes/*deployment*.y*ml
---
+++ jenkins/agent/setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+check_exit_failure()
+{
+    if [ $? -ne 0 ]
+    then
+        echo "$1" 1>&2
+        sleep 10
+        exit 1
+    fi
+}
+
+# Check if Jenkins is ready
+curl -s http://eip-deploy_jenkins-controller:$JENKINS_PORT | grep "Authentication required"
+check_exit_failure "Jenkins not ready"
+
+# Get agent executable
+curl http://eip-deploy_jenkins-controller:$JENKINS_PORT/jnlpJars/agent.jar --output /app/agent.jar
+check_exit_failure "Fail to download agent"
+
+# Get agent secret
+secret=`curl -L -s -u "Jenkins Agent:cc9c494ca8afcc43a0db7e8aa65662f6" -X GET http://eip-deploy_jenkins-controller:4242/computer/Nuage%20Malin%20Agent/jenkins-agent.jnlp | sed "s/.*<application-desc main-class=\"hudson.remoting.jnlp.Main\"><argument>\([a-z0-9]*\).*/\1/"`
+check_exit_failure "Fail to get agent secret"
+
+# Start agent
+java -jar /app/agent.jar -jnlpUrl http://eip-deploy_jenkins-controller:$JENKINS_PORT/computer/Nuage%20Malin%20Agent/jenkins-agent.jnlp -secret $secret
+check_exit_failure "Fail to run agent"
