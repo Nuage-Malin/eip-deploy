13c915b0df67748bdc32d914d459593927f18391
Vincent Andrieu <vincent.andrieu@epitech.eu>
2022-10-03 19:29:13 -0400

    Fix get agent secret with new Jenkins version Fix syntax with docker compose V2

--- jenkins/agent/setup.sh
+++ jenkins/agent/setup.sh
@@ -11,17 +11,17 @@ check_exit_failure()
 }
 
 # Check if Jenkins is ready
-curl -s http://eip-deploy_jenkins-controller:$JENKINS_PORT | grep "Authentication required"
+curl -s http://eip-deploy-jenkins-controller:$JENKINS_PORT | grep "Authentication required"
 check_exit_failure "Jenkins not ready"
 
 # Get agent executable
-curl http://eip-deploy_jenkins-controller:$JENKINS_PORT/jnlpJars/agent.jar --output /app/agent.jar
+curl http://eip-deploy-jenkins-controller:$JENKINS_PORT/jnlpJars/agent.jar --output /app/agent.jar
 check_exit_failure "Fail to download agent"
 
 # Get agent secret
-secret=`curl -L -s -u "Jenkins Agent:cc9c494ca8afcc43a0db7e8aa65662f6" -X GET http://eip-deploy_jenkins-controller:4242/computer/Nuage%20Malin%20Agent/jenkins-agent.jnlp | sed "s/.*<application-desc main-class=\"hudson.remoting.jnlp.Main\"><argument>\([a-z0-9]*\).*/\1/"`
+secret=`curl -L -s -u "Jenkins Agent:cc9c494ca8afcc43a0db7e8aa65662f6" -X GET http://eip-deploy-jenkins-controller:4242/computer/Nuage%20Malin%20Agent/jenkins-agent.jnlp | sed "s/.*<application-desc><argument>\([a-z0-9]*\).*/\1/"`
 check_exit_failure "Fail to get agent secret"
 
 # Start agent
-java -jar /app/agent.jar -jnlpUrl http://eip-deploy_jenkins-controller:$JENKINS_PORT/computer/Nuage%20Malin%20Agent/jenkins-agent.jnlp -secret $secret
+java -jar /app/agent.jar -jnlpUrl http://eip-deploy-jenkins-controller:$JENKINS_PORT/computer/Nuage%20Malin%20Agent/jenkins-agent.jnlp -secret $secret
 check_exit_failure "Fail to run agent"
